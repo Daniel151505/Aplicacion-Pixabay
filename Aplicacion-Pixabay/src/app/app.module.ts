fe4c5cec51db0deaf0dab5ff9c6d77c6fa9d8ab9
Daniel151505 <daniel150520011@gmail.com>
2021-02-23 17:39:04 -0500

    Agregando Componente Spinner

--- Aplicacion-Pixabay/src/app/app.module.ts
+++ Aplicacion-Pixabay/src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { BuscarImagenComponent } from './components/buscar-imagen/buscar-imagen.component';
 import { ListarImagenComponent } from './components/listar-imagen/listar-imagen.component';
 import { ErrorComponent } from './shared/error/error.component';
+import { SpinnerComponent } from './shared/spinner/spinner.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     BuscarImagenComponent,
     ListarImagenComponent,
-    ErrorComponent
+    ErrorComponent,
+    SpinnerComponent
   ],
   imports: [
     BrowserModule,
