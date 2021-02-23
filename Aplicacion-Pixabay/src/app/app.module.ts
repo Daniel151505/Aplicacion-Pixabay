7f9430ae10399df08c4835fb5d6cf4a99fa4f606
Daniel151505 <daniel150520011@gmail.com>
2021-02-23 17:35:08 -0500

    Agregando Componente Listar Imagen

--- Aplicacion-Pixabay/src/app/app.module.ts
+++ Aplicacion-Pixabay/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BuscarImagenComponent } from './components/buscar-imagen/buscar-imagen.component';
+import { ListarImagenComponent } from './components/listar-imagen/listar-imagen.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    BuscarImagenComponent
+    BuscarImagenComponent,
+    ListarImagenComponent
   ],
   imports: [
     BrowserModule,
