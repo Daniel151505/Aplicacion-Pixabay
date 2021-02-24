79867cbc1209b4a741b935b2adba9b00d92d50ad
Daniel151505 <daniel150520011@gmail.com>
2021-02-23 20:36:34 -0500

    Agregar Metodo Get Imagenes

--- Aplicacion-Pixabay/src/app/services/imagen.service.ts
+++ Aplicacion-Pixabay/src/app/services/imagen.service.ts
@@ -28,4 +28,10 @@ export class ImagenService {
     return this.terminoBusqueda$.asObservable()
   }
 
+  getImagens(termino: string):Observable<any> {
+    const KEY = '20267393-f8e01530e94512469675bd333'
+    const URL= 'https://pixabay.com/api/?key=' + KEY + '&q=' + termino
+    return this.http.get(URL)
+  }
+
 }
