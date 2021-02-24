d305029d9198e60dff06f9f7374d96d4a6b5cf3f
Daniel151505 <daniel150520011@gmail.com>
2021-02-23 19:23:49 -0500

    Importando Imagen Service

--- Aplicacion-Pixabay/src/app/components/buscar-imagen/buscar-imagen.component.ts
+++ Aplicacion-Pixabay/src/app/components/buscar-imagen/buscar-imagen.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ImagenService } from 'src/app/services/imagen.service';
 
 @Component({
   selector: 'app-buscar-imagen',
@@ -7,7 +8,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BuscarImagenComponent implements OnInit {
 
-  constructor() { }
+  nombreImagen: string = ''
+
+  constructor( private imagenSerive: ImagenService ) { }
 
   ngOnInit(): void {
   }
